54091e4669e579722b6ce6b59bf60cd7c620ebb0
Shubhamraaj558 <shubhamraaj558@gmail.com>
2026-09-05 10:11:53 +0530

    Fix displayINRCurrency undefined match crash

--- frontend/src/helpers/displayCurrency.js
+++ frontend/src/helpers/displayCurrency.js
@@ -1,12 +1,16 @@
 const displayINRCurrency = (num) => {
-    const formatter = new Intl.NumberFormat('en-IN',{
-        style : "currency",
-        currency : 'INR',
-        minimumFractionDigits : 2
+    // Agar num undefined ya null hai toh crash hone se bachne ke liye 0 pass kar do
+    if (!num) {
+        num = 0;
+    }
+
+    const formatter = new Intl.NumberFormat('en-IN', {
+        style: "currency",
+        currency: 'INR',
+        minimumFractionDigits: 2
     })
 
     return formatter.format(num)
-
 }
 
 export default displayINRCurrency
