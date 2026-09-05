5f8741df6a272d4f954e519f5f2189d6e0538bf2
Shubhamraaj558 <shubhamraaj558@gmail.com>
2026-09-05 18:18:35 +0530

    cloudinary env and stripe fallback fix.

--- frontend/src/pages/Success.js
+++ frontend/src/pages/Success.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import React, { useEffect } from 'react';
 import SUCCESSIMAGE from '../assest/success.gif';
 import { Link } from 'react-router-dom';
 import { FaArrowLeft, FaCheckCircle, FaShoppingBag } from 'react-icons/fa';
